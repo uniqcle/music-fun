a21c4d4c736e62b8508585cab137df703658f4ca
Andrey <uniqcle@yandex.ru>
2026-02-18 10:01:05 +0700

    track detail

--- src/components/TrackDetail.tsx
+++ src/components/TrackDetail.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 
 export default function TrackDetail({ selectedTrack, isFetching, setIsFetching }) {
-    useEffect(() => {
+    
+	useEffect(() => {
         if (!selectedTrack) {
             return;
         }
